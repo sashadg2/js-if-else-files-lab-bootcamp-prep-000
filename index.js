4f3e332b9463f150e7ca7803e10f8038e594abab
Oleksandr Diuriagin <sashadg@gmail.com>
2018-01-25 02:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var height = "74"
 module.exports = { name, height
 }
 
-if name = "Jack" {
+if (name = "Jack") {
 console.log("That's correct")
 } else {
   console.log("Your name was supposed to be Jack")
